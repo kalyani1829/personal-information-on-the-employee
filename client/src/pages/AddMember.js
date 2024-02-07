324af59c55a8f037fce29184576a74007dd3418d
kalyani1829 <infinite.wendev@gmail.com>
2024-02-07 16:13:36 +0530

    Added Add User form

---
+++ client/src/pages/AddMember.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+const AddMembers = () => {
+
+    // states
+    const [userData, setUserData] = useState({})
+
+    const handleInputs = (e) => {
+        console.log(e);
+        setUserData({ ...userData, [e.target.name]: e.target.value
+    })
+};
+    const addUser = (e) => {
+        e.preventDefault();
+        console.log('user data: ', userData);
+    }
+
+
+
+    return(
+        <>
+            <h3>Add Members</h3>
+            <div className="memberForm">
+                <form onSubmit = {(e) => addUser(e)}>
+                    <input 
+                        type="text"
+                        placeholder="Full Name"
+                        onChange = {handleInputs}
+                        name = "userFullName"
+                    /><br/> 
+                    <input 
+                        type="email"
+                        placeholder="Email Address"
+                        onChange = {handleInputs}
+                        name = "userEmail"
+                    /><br/> 
+                    <input 
+                        type="text"
+                        placeholder="Contact Number"
+                        onChange = {handleInputs}
+                        name = "userPhone"
+                    /> <br/> <br/> 
+                    <button type="submit">Add</button>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default AddMembers;
